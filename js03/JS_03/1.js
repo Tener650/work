824571bfb49fffefb77ae842b229b14406b7b4e7
rud <C:\Users\rud\AppData\Roaming\The Bat!>
2018-12-17 16:21:13 +0200

    added test.txt file Signed-off-by: Grem <tener650@gmail.com>

---
+++ js03/JS_03/1.js
@@ -0,0 +1,12 @@
+function pow2() {
+    //document.getElementById('inp1').value
+    //alert();
+    //alert(5);
+    //alert('hello');
+    var num1; //переменная
+    num1 = document.getElementById('inp1').value;
+    console.log(num1 * num1);
+    num1 = 7;
+    var b = 5;
+    alert (num1 + b);
+}
